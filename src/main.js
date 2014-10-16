1d981848118538f108f602a847c3084d34433927
dengxinxin <dengxinxin@baidu.com>
2014-10-16 11:59:46 +0800

    add doc and comments

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /**
- * Created by exodia on 14-4-14.
+ * @file IoC 类
+ * @author exodia (d_xinxin@163.com)
  */
-
 void function (define, global, undefined) {
     define(
         function (require) {
@@ -15,10 +15,21 @@ void function (define, global, undefined) {
                 return creator.apply(this, args);
             };
 
-            function Context(config) {
+            /**
+             * IoC 容器类，根据配置实例化一个 IoC 容器
+             * @class IoC
+             *
+             * @param {Object} [config] IoC 配置
+             * @param {Function} [config.loader=require] 符合 AMD 规范的模块加载器，默认为全局的 require
+             * @param {Object.<string, ComponentConfig>} [config.components]
+             * 批量配置构件, 其中每个key 为构件 id，值为构建配置对象，配置选项见 @link IoC#addComponent
+             *
+             * @returns {IoC}
+             */
+            function IoC(config) {
                 config = config || {};
-                if (!(this instanceof Context)) {
-                    return new Context(config);
+                if (!(this instanceof IoC)) {
+                    return new IoC(config);
                 }
 
                 this.moduleLoader = config.loader || globalLoader;
@@ -29,43 +40,66 @@ void function (define, global, undefined) {
             }
 
             /**
-             * 向容器中注册构件，配置中，args 和 properties 中的每个元素，可以使用 $ref 操作符：
-             *      {
-             *        args: [ { $ref: 'otherComponent' } ]
-             *     }
+             * 构件配置对象
              *
-             * 容器会解析第一层的$ref，从值中获取对应的实例，若实例未注册，返回 null
-             *
-             * @param {String | Object} id
-             * @param {Object} [config]
-             * @param {Function | String} config.creator 创建构件的函数或模块名称
-             * @param {Boolean=false} config.isFactory 是否为工厂函数，默认false，会通过 new 方式调用，true 时直接调用
-             * @param {'transient' | 'singleton' | 'static'} [config.scope = 'transient']
-             * 构件作用域，默认为 transient，每次获取构件，都会新建一个实例返回，若为 singleton，则会返回同一个实例
-             * 若为 static，则直接返回creator
-             *
-             * @param {Array} config.args 传递给创建构件函数的参数
-             * @param {Object} config.properties 附加给实例的属性
-             *      ioc.addComponent('List', {
-             *          // 构造函数创建构件 new creator, 或者字符串，字符串则为 amd 模块名
-             *          creator: require('./List'),
-             *          scope: 'transient',
-             *          args: [
-             *              {
-             *                   $ref: 'entityName'
-             *              }
-             *          ],
-             *
-             *          // 属性注入， 不设置$setter, 则直接instance.xxx = xxx
-             *          properties: {
-             *              model: { $ref: 'ListModel' },
-             *              view: { $ref: 'ListView' },
-             *              name: 'xxxx'
-             *          }
-             *      });
-             *
+             * @typedef {Object} ComponentConfig
+             * @property {Function | string} creator 创建构件的函数或模块名称
+             * @property {boolean} [isFactory=false] 是否为工厂函数，默认false，会通过 new 方式调用，true 时直接调用
+             * @property {'transient' | 'singleton' | 'static'} [scope='transient']
+             * 构件作用域，默认为 transient，每次获取构件，都会新建一个实例返回，若为 singleton，则会返回同一个实例，若为 static，则直接返回creator
+             * @property {DependencyConfig[]} args 传递给构件构造函数的参数，
+             * 获取构件时，根据 args 的配置，自动创建其依赖，作为构造函数参数传入
+             * @property {Object.<string, DependencyConfig>} [properties] 附加给构件实例的属性，
+             * 获取构件时，IoC 会根据 properties 的配置，自动创建其依赖， 作为属性注入构件实例。
+             * **note:** 若构件实例存在 ```set + 属性名首字母大些的方法```，则会调用此方法，并将依赖传入，
+             * 否则简单的调用 ```this.{propertyName} = {property}```
              */
-            Context.prototype.addComponent = function (id, config) {
+
+            /**
+             * 构件依赖配置对象，用于配置构件的依赖，若未配置$ref与$import，则本身作为依赖值，否则将根据$ref/$import的声明查找依赖。
+             *
+             * @typedef {* | Object} DependencyConfig
+             * @mixes ComponentConfig
+             *
+             * @property {string} [$ref] 声明依赖的构件，获取构件时，会自动创建其依赖的构件，作为构造函数参数传入
+             * @property {string} [$import] 导入指定构件的配置，将创建一个匿名构件配置，其余的配置将覆盖掉导入的配置
+             */
+
+            /**
+             *
+             * 向容器中注册构件
+             *
+             * @method IoC#addComponent
+             * @param {String | ComponentConfig} id
+             * @param {ComponentConfig} [config]
+             * @example
+             * ioc.addComponent('list', {
+             *     // 构造函数创建构件 new creator, 或者字符串，字符串则为 amd 模块名
+             *     creator: require('./List'),
+             *     scope: 'transient',
+             *     args: [{$ref: 'entityName'}],
+             *
+             *     // 属性注入， 不设置$setter, 则直接instance.xxx = xxx
+             *     properties: {
+             *          model: {$ref: 'listModel'},
+             *          view: {$ref: 'listView'},
+             *          name: 'xxxx' // 未设置$ref/$import操作符，'xxxx' 即为依赖值
+             *     }
+             * });
+             *
+             * ioc.addComponent('listData', {
+             *     creator: 'ListDatal',
+             *     scope: 'transient',
+             *
+             *     properties: {
+             *          data: {
+             *              $import: 'requestStrategy', // 创建匿名组件，默认继承 requestStrategy 的配置，
+             *              args:['list', 'list'] // 重写 requestStrategy 的 args 配置
+             *          },
+             *     }
+             * });
+             */
+            IoC.prototype.addComponent = function (id, config) {
                 var ids = [];
                 if (typeof id === 'string') {
                     var conf = {};
@@ -91,7 +125,22 @@ void function (define, global, undefined) {
                 }
             };
 
-            Context.prototype.getComponent = function (ids, cb) {
+
+            /**
+             * 获取构件实例成功后的回调函数
+             *
+             * @callback getComponentCallback
+             * @param {...*} component 获取的构件实例，顺序对应传入的 id 顺序
+             */
+            /**
+             * 获取构件实例
+             *
+             * @method IoC#getComponent
+             * @param {string | string[]} ids 构件 id，数组或者字符串
+             * @param {getComponentCallback} cb 获取构件成功后的回调函数，构件将按 id 的顺序依次作为参数传入
+             * @returns {IoC}
+             */
+            IoC.prototype.getComponent = function (ids, cb) {
                 ids = ids instanceof Array ? ids : [ids];
                 var needModules = {};
                 var me = this;
@@ -112,18 +161,26 @@ void function (define, global, undefined) {
                 return this;
             };
 
-            Context.prototype.getComponentConfig = function (id) {
+            IoC.prototype.getComponentConfig = function (id) {
                 return this.components[id];
             };
 
-            Context.prototype.loader = function (loader) {
+            /**
+             * 设置 IoC 的模块加载器
+             *
+             * @method IoC#loader
+             * @param {Function} loader 符合 AMD 规范的模块加载器
+             */
+            IoC.prototype.loader = function (loader) {
                 this.moduleLoader = loader;
             };
 
             /**
              * 销毁容器，会遍历容器中的单例，如果有设置dispose，调用他们的 dispose 方法
+             *
+             * @method IoC#dispose
              */
-            Context.prototype.dispose = function () {
+            IoC.prototype.dispose = function () {
                 this.container.dispose();
                 this.components = null;
                 this.parser = null;
@@ -325,7 +382,7 @@ void function (define, global, undefined) {
                 typeof instance[name] === 'function' ? instance[name](value) : (instance[key] = value);
             }
 
-            return Context;
+            return IoC;
         }
     );
 
